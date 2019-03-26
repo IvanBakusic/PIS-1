dbcb05edf10dd498604538429da1b8ee519c1ea3
TBresk <44880026+TBresk@users.noreply.github.com>
2019-03-26 16:24:24 +0100

    Ivan Program.cs
    Dodano "i Tomo"

--- IvanBakusic/Program.cs
+++ IvanBakusic/Program.cs
@@ -9,7 +9,7 @@ namespace IvanBakusic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ja sam Ivan.");
+            Console.WriteLine("Ja sam Ivan i Tomo.");
         }
     }
 }
